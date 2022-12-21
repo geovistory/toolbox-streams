a5a09323b10ff68a87e75a9fcd2013eabb7aad59
joschne <jonas.schneider@balcab.ch>
2022-12-21 17:50:18 +0100

    fix

--- app/src/main/java/org/geovistory/toolbox/streams/app/DbTopicNames.java
+++ app/src/main/java/org/geovistory/toolbox/streams/app/DbTopicNames.java
@@ -5,7 +5,7 @@ import org.geovistory.toolbox.streams.lib.Utils;
 public enum DbTopicNames {
     pro_projects("projects.project"),
     pro_text_property("projects.text_property"),
-    pro_dfh_profile_proj_rel("dfh_profile_proj_rel"),
+    pro_dfh_profile_proj_rel("projects.dfh_profile_proj_rel"),
     sys_config("system.config"),
     dfh_api_property("data_for_history.api_property"),
     dfh_api_class("data_for_history.api_class");
