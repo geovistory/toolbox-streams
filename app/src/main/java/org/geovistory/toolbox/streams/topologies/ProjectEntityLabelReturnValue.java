9a2081063575344353fed6cfc3632c75a76e6bde
joschne <jonas.schneider@balcab.ch>
2023-01-19 13:45:09 +0100

    Initialize project entity label with changed=true

--- app/src/main/java/org/geovistory/toolbox/streams/topologies/ProjectEntityLabelReturnValue.java
+++ app/src/main/java/org/geovistory/toolbox/streams/topologies/ProjectEntityLabelReturnValue.java
@@ -2,11 +2,9 @@ package org.geovistory.toolbox.streams.topologies;
 
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KTable;
 import org.geovistory.toolbox.streams.avro.ProjectEntityKey;
 import org.geovistory.toolbox.streams.avro.ProjectEntityLabelValue;
 
 public record ProjectEntityLabelReturnValue(StreamsBuilder builder,
-                                            KTable<ProjectEntityKey, ProjectEntityLabelValue> projectTopStatementTable,
                                             KStream<ProjectEntityKey, ProjectEntityLabelValue> projectTopStatementStream) {
 }
