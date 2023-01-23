d65159bd663221a4bc1afb85876bf01a04bf20cd
joschne <jonas.schneider@balcab.ch>
2023-01-23 14:27:15 +0100

    CLI > add --delete-topics flag

--- utilities/src/main/java/org/geovistory/toolbox/streams/utilities/cli/CLI.java
+++ utilities/src/main/java/org/geovistory/toolbox/streams/utilities/cli/CLI.java
@@ -5,18 +5,26 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.apache.commons.cli.*;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.geovistory.toolbox.streams.lib.AppConfig;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class CLI {
 
     public static final Option OPT_PRINT_SCHEMA_REGISTRY_URL = new Option("u", "print-url", false, "print schema registry url");
     public static final Option OPT_HELP = new Option("h", "help", false, "print help");
-    public static final Option OPT_LIST = new Option("s", "list-schemas", false, "list schemas of schema registry");
-    public static final Option OPT_DELETE = new Option("d", "delete-schemas", true, "soft-delete schemas of schema registry containing the provided <arg> string. This will not delete unless you provide the --confirm flag.");
+    public static final Option OPT_LIST_SCHEMAS = new Option("s", "list-schemas", false, "list schemas of schema registry");
+    public static final Option OPT_DELETE_SCHEMAS = new Option("d", "delete-schemas", true, "soft-delete schemas of schema registry containing the provided <arg> string. This will not delete unless you provide the --confirm flag.");
+    public static final Option OPT_LIST_TOPICS = new Option(null, "list-topics", false, "list topics");
+    public static final Option OPT_DELETE_TOPICS = new Option(null, "delete-topics", true, "delete topics containing the provided <arg> string. This will not delete unless you provide the --confirm flag.");
     public static final Option OPT_CONFIRM = new Option(null, "confirm", false, "Confirm performing a dangerous opteration.");
 
     public static void printHelp(Options options) {
@@ -38,8 +46,10 @@ public class CLI {
         Options options = new Options();
         options.addOption(OPT_PRINT_SCHEMA_REGISTRY_URL);
         options.addOption(OPT_HELP);
-        options.addOption(OPT_LIST);
-        options.addOption(OPT_DELETE);
+        options.addOption(OPT_LIST_SCHEMAS);
+        options.addOption(OPT_DELETE_SCHEMAS);
+        options.addOption(OPT_LIST_TOPICS);
+        options.addOption(OPT_DELETE_TOPICS);
         options.addOption(OPT_CONFIRM);
 
 
@@ -48,17 +58,26 @@ public class CLI {
             CommandLine line = parser.parse(options, args);
             if (line.hasOption(OPT_PRINT_SCHEMA_REGISTRY_URL.getLongOpt())) {
                 printSchemaRegistryURL();
-            } else if (line.hasOption(OPT_LIST.getLongOpt())) {
+            } else if (line.hasOption(OPT_LIST_SCHEMAS.getLongOpt())) {
                 var schemas = getSchemas();
                 schemas.forEach(System.out::println);
                 System.out.println(schemas.size() + " schemas found.");
-            } else if (line.hasOption(OPT_DELETE.getLongOpt())) {
+            } else if (line.hasOption(OPT_DELETE_SCHEMAS.getLongOpt())) {
                 Boolean confirmed = line.hasOption(OPT_CONFIRM.getLongOpt());
-                deleteSchemas(line.getOptionValue(OPT_DELETE.getLongOpt()), confirmed);
+                deleteSchemas(line.getOptionValue(OPT_DELETE_SCHEMAS.getLongOpt()), confirmed);
+            } else if (line.hasOption(OPT_LIST_TOPICS.getLongOpt())) {
+                var topics = getTopics();
+                topics.forEach(System.out::println);
+                System.out.println(topics.size() + " topics found.");
+
+            } else if (line.hasOption(OPT_DELETE_TOPICS.getLongOpt())) {
+                Boolean confirmed = line.hasOption(OPT_CONFIRM.getLongOpt());
+                deleteTopics(line.getOptionValue(OPT_DELETE_TOPICS.getLongOpt()), confirmed);
             } else {
                 printHelp(options);
             }
-        } catch (ParseException | IOException exp) {
+        } catch (ParseException |
+                 IOException exp) {
             System.out.println("Unexpected exception:" + exp.getMessage());
         }
     }
@@ -77,12 +96,52 @@ public class CLI {
                 .method("GET", null)
                 .build();
         Response response = client.newCall(request).execute();
+        assert response.body() != null;
         var s = response.body().string();
         ObjectMapper objectMapper = new ObjectMapper();
         StringArrayList schemas = objectMapper.readValue(s, StringArrayList.class);
         response.close();
         return schemas;
     }
+
+    public static List<String> getTopics() {
+        AdminClient adminClient = getAdminClient();
+        var topicList = adminClient.listTopics();
+        try {
+            return topicList.names().get().stream().toList();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static AdminClient getAdminClient() {
+        Properties props = new Properties();
+        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, AppConfig.INSTANCE.getKafkaBootstrapServers());
+        return AdminClient.create(props);
+    }
+
+    public static void deleteTopics(String prefix, Boolean confirmed) {
+        AdminClient adminClient = getAdminClient();
+        var topics = getTopics();
+        var filtered = topics.stream().filter(s -> s.contains(prefix)).toList();
+        filtered.forEach(System.out::println);
+        if (confirmed) {
+            var res = adminClient.deleteTopics(filtered);
+            try {
+                res.all().get();
+                System.out.println("Successfully deleted: " + filtered.size());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+
+        }
+        else{
+            System.out.println("To delete the above " + filtered.size() + " topics add the flag --confirm");
+        }
+    }
+
 
     public static void deleteSchemas(String prefix, Boolean confirmed) throws IOException {
         var schemas = getSchemas();
