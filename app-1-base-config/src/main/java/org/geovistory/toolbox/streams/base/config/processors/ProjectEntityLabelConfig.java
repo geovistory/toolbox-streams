13539822efda87837b2d78255e4ed9cf7211d620
joschne <jonas.schneider@balcab.ch>
2023-03-09 10:35:06 +0100

    fix handling of deleted label configs

--- app-1-base-config/src/main/java/org/geovistory/toolbox/streams/base/config/processors/ProjectEntityLabelConfig.java
+++ app-1-base-config/src/main/java/org/geovistory/toolbox/streams/base/config/processors/ProjectEntityLabelConfig.java
@@ -5,16 +5,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.projects.entity_label_config.Key;
 import dev.projects.entity_label_config.Value;
 import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.processor.ProcessorContext;
 import org.apache.kafka.streams.state.KeyValueStore;
+import org.apache.kafka.streams.state.StoreBuilder;
+import org.apache.kafka.streams.state.Stores;
 import org.geovistory.toolbox.streams.avro.*;
-import org.geovistory.toolbox.streams.lib.ConfluentAvroSerdes;
-import org.geovistory.toolbox.streams.lib.Utils;
 import org.geovistory.toolbox.streams.base.config.DbTopicNames;
 import org.geovistory.toolbox.streams.base.config.RegisterInnerTopic;
 import org.geovistory.toolbox.streams.base.config.RegisterInputTopic;
+import org.geovistory.toolbox.streams.lib.ConfluentAvroSerdes;
+import org.geovistory.toolbox.streams.lib.Utils;
+
+import java.util.Collections;
+import java.util.Set;
 
 
 public class ProjectEntityLabelConfig {
@@ -42,23 +49,23 @@ public class ProjectEntityLabelConfig {
             KStream<Key, Value> proEntityLabelConfigStream,
             KTable<CommunityEntityLabelConfigKey, CommunityEntityLabelConfigValue> communityEntityLabelConfigTable
     ) {
-        var mapper = new ObjectMapper(); // create once, reuse
         var avroSerdes = new ConfluentAvroSerdes();
         /* STREAM PROCESSORS */
         // 2)
 
-        var configByProjectClassKey = proEntityLabelConfigStream.selectKey(
-                        (key, value) -> ProjectClassKey.newBuilder()
-                                .setProjectId(value.getFkProject())
-                                .setClassId(value.getFkClass())
-                                .build(),
-                        Named.as(inner.TOPICS.project_entity_label_config_by_project_class)
+        var configByProjectClassKey = proEntityLabelConfigStream
+                .transform(new TransformSupplier("handle_project_entity_label_config_deletes"))
+                .repartition(
+                        Repartitioned.<ProjectClassKey, ProjectEntityLabelConfigValue>as(inner.TOPICS.project_entity_label_config_by_project_class + "-repartition")
+                                .withKeySerde(avroSerdes.ProjectClassKey())
+                                .withValueSerde(avroSerdes.ProjectEntityLabelConfigValue())
+                                .withName(inner.TOPICS.project_entity_label_config_by_project_class + "-repartition")
                 )
                 .toTable(
                         Named.as(inner.TOPICS.project_entity_label_config_by_project_class + "-to-table"),
-                        Materialized.<ProjectClassKey, Value, KeyValueStore<Bytes, byte[]>>as(inner.TOPICS.project_entity_label_config_by_project_class)
+                        Materialized.<ProjectClassKey, ProjectEntityLabelConfigValue, KeyValueStore<Bytes, byte[]>>as(inner.TOPICS.project_entity_label_config_by_project_class)
                                 .withKeySerde(avroSerdes.ProjectClassKey())
-                                .withValueSerde(avroSerdes.ProEntityLabelConfigValue())
+                                .withValueSerde(avroSerdes.ProjectEntityLabelConfigValue())
                 );
 
         // 2
@@ -68,21 +75,15 @@ public class ProjectEntityLabelConfig {
                     var result = ProjectEntityLabelConfigValue.newBuilder()
                             .setProjectId(value1.getProjectId())
                             .setClassId(value1.getClassId())
-
                             .build();
                     // if we have a project configuration, take it
                     if (value2 != null) {
-                        result.setDeleted$1(Utils.stringIsEqualTrue(value2.getDeleted$1()));
-                        try {
-                            var config = mapper.readValue(value2.getConfig(), EntityLabelConfig.class);
-                            result.setConfig(config);
-                        } catch (JsonProcessingException e) {
-                            e.printStackTrace();
-                        }
+                        result.setDeleted$1(value2.getDeleted$1());
+                        result.setConfig(value2.getConfig());
                     }
                     return result;
                 },
-                Named.as(inner.TOPICS.project_class_with_project_label_config+ "-fk-left-join"),
+                Named.as(inner.TOPICS.project_class_with_project_label_config + "-fk-left-join"),
                 Materialized.<ProjectClassKey, ProjectEntityLabelConfigValue, KeyValueStore<Bytes, byte[]>>as(inner.TOPICS.project_class_with_project_label_config)
                         .withKeySerde(avroSerdes.ProjectClassKey())
                         .withValueSerde(avroSerdes.ProjectEntityLabelConfigValue())
@@ -124,9 +125,9 @@ public class ProjectEntityLabelConfig {
         ).to(
                 output.TOPICS.project_entity_label_config,
                 Produced.with(
-                        avroSerdes.ProjectClassKey(),
-                        avroSerdes.ProjectEntityLabelConfigValue()
-                )
+                                avroSerdes.ProjectClassKey(),
+                                avroSerdes.ProjectEntityLabelConfigValue()
+                        )
                         .withName(output.TOPICS.project_entity_label_config + "-producer")
         );
 
@@ -152,5 +153,110 @@ public class ProjectEntityLabelConfig {
         TOPICS;
         public final String project_entity_label_config = Utils.tsPrefixed("project_entity_label_config");
     }
+
+
+    public static class TransformSupplier implements TransformerSupplier<
+            dev.projects.entity_label_config.Key, dev.projects.entity_label_config.Value,
+            KeyValue<ProjectClassKey, ProjectEntityLabelConfigValue>> {
+
+        private final String stateStoreName;
+        private final ConfluentAvroSerdes avroSerdes = new ConfluentAvroSerdes();
+
+        TransformSupplier(String stateStoreName) {
+            this.stateStoreName = stateStoreName;
+        }
+
+        @Override
+        public Transformer<dev.projects.entity_label_config.Key, dev.projects.entity_label_config.Value, KeyValue<ProjectClassKey, ProjectEntityLabelConfigValue>> get() {
+            return new ProjectEntityLabelConfig.Transform(stateStoreName);
+        }
+
+        @Override
+        public Set<StoreBuilder<?>> stores() {
+            StoreBuilder<KeyValueStore<dev.projects.entity_label_config.Key, ProjectEntityLabelConfigValue>> keyValueStoreBuilder =
+                    Stores.keyValueStoreBuilder(Stores.persistentKeyValueStore(stateStoreName),
+                            avroSerdes.ProEntityLabelConfigKey(),
+                            avroSerdes.ProjectEntityLabelConfigValue());
+            return Collections.singleton(keyValueStoreBuilder);
+        }
+    }
+
+    public static class Transform implements Transformer<
+            dev.projects.entity_label_config.Key, dev.projects.entity_label_config.Value,
+            KeyValue<ProjectClassKey, ProjectEntityLabelConfigValue>> {
+
+        private final String stateStoreName;
+        private KeyValueStore<dev.projects.entity_label_config.Key, ProjectEntityLabelConfigValue> kvStore;
+
+        private final ObjectMapper mapper = new ObjectMapper(); // create once, reuse
+
+        public Transform(String stateStoreName) {
+            this.stateStoreName = stateStoreName;
+        }
+
+        @Override
+        public void init(ProcessorContext context) {
+            this.kvStore = context.getStateStore(stateStoreName);
+        }
+
+        @Override
+        public KeyValue<ProjectClassKey, ProjectEntityLabelConfigValue> transform(
+                dev.projects.entity_label_config.Key key,
+                dev.projects.entity_label_config.Value value
+        ) {
+
+            // if the item was deleted
+            if (value == null || Utils.stringIsEqualTrue(value.getDeleted$1())) {
+                // get existing value
+                var existing = kvStore.get(key);
+                if (existing == null) return null;
+
+                // mark as deleted
+                existing.setDeleted$1(true);
+
+                // push downstream
+                return KeyValue.pair(
+                        ProjectClassKey.newBuilder()
+                                .setClassId(existing.getClassId())
+                                .setProjectId(existing.getProjectId())
+                                .build(),
+                        existing
+                );
+            }
+
+            // if item was not deleted
+            try {
+                EntityLabelConfig config = mapper.readValue(value.getConfig(), EntityLabelConfig.class);
+                var k = ProjectClassKey.newBuilder()
+                        .setClassId(value.getFkClass())
+                        .setProjectId(value.getFkProject())
+                        .build();
+                var v = ProjectEntityLabelConfigValue.newBuilder()
+                        .setClassId(value.getFkClass())
+                        .setProjectId(value.getFkProject())
+                        .setConfig(config)
+                        .setDeleted$1(Utils.stringIsEqualTrue(value.getDeleted$1()))
+                        .build();
+
+                // add to local store
+                kvStore.put(key, v);
+
+                // push downstream
+                return KeyValue.pair(k, v);
+
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+
+        }
+
+        public void close() {
+
+        }
+
+    }
+
 
 }
