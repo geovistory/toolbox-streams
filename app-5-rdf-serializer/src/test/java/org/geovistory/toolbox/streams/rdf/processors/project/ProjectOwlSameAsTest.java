fc5c0b3b4f963429f2f2cf1140a99ce93cea2772
Djamel Ferhod <djamel.ferhod@ish-lyon.cnrs.fr>
2023-09-25 12:33:28 +0200

    Added more unit tests, all passed.

--- app-5-rdf-serializer/src/test/java/org/geovistory/toolbox/streams/rdf/processors/project/ProjectOwlSameAsTest.java
+++ app-5-rdf-serializer/src/test/java/org/geovistory/toolbox/streams/rdf/processors/project/ProjectOwlSameAsTest.java
@@ -8,8 +8,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Properties;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -120,4 +118,247 @@ class ProjectOwlSameAsTest {
         assertThat(outputTopic.isEmpty()).isFalse();
     }
 
+    /**
+     * The goal of this test is to check if the output has the correct size: 1 (one record for one sameAs)
+     */
+    @Test
+    void testSizeOfOutput() {
+
+        var projectIdLeft = 1;
+        var subjectIdLeft = "i1";
+        var statementIdLeft = 111;
+        var objectIdLeft = "i200";
+        var propertyIdLeft = 1943;
+
+        var projectIdRight = 1;
+        var subjectIdRight = "i200";
+        var statementIdRight = 999;
+        var objectAppellationStringRight = "https://wikidata/i345";
+        var propertyIdRight = 1843;
+
+        var kLeft = ProjectStatementKey.newBuilder().setProjectId(projectIdLeft).setStatementId(statementIdLeft).build();
+        var vLeft = ProjectStatementValue.newBuilder()
+                .setProjectId(projectIdLeft)
+                .setStatementId(statementIdLeft)
+                .setStatement(
+                        StatementEnrichedValue.newBuilder()
+                                .setSubjectId(subjectIdLeft)
+                                .setObjectId(objectIdLeft)
+                                .setPropertyId(propertyIdLeft)
+                                .build())
+                .setDeleted$1(false)
+                .build();
+        projectStatementWithEntityTopic.pipeInput(kLeft, vLeft);
+
+        var kRight = ProjectStatementKey.newBuilder().setProjectId(projectIdRight).setStatementId(statementIdRight).build();
+        var vRight = ProjectStatementValue.newBuilder()
+                .setProjectId(projectIdRight)
+                .setStatementId(statementIdRight)
+                .setStatement(
+                        StatementEnrichedValue.newBuilder()
+                                .setSubjectId(subjectIdRight)
+                                .setObject(NodeValue.newBuilder()
+                                        .setClassId(0)
+                                        .setAppellation(Appellation.newBuilder()
+                                                .setFkClass(0)
+                                                .setString(objectAppellationStringRight)
+                                                .build()
+                                        ).build()
+                                )
+                                .setPropertyId(propertyIdRight)
+                                .build())
+                .setDeleted$1(false)
+                .build();
+        projectStatementWithLiteralTopic.pipeInput(kRight, vRight);
+
+        var outRecords = outputTopic.readKeyValuesToMap();
+        assertThat(outRecords).hasSize(1);
+    }
+
+    /**
+     * The goal of this test is to check if the operation value "delete" is correctly set in the output topic
+     */
+    @Test
+    void testOperationValueDelete() {
+
+        var projectIdLeft = 1;
+        var subjectIdLeft = "i1";
+        var statementIdLeft = 111;
+        var objectIdLeft = "i200";
+        var propertyIdLeft = 1943;
+
+        var projectIdRight = 1;
+        var subjectIdRight = "i200";
+        var statementIdRight = 999;
+        var objectAppellationStringRight = "https://wikidata/i345";
+        var propertyIdRight = 1843;
+
+        var kLeft = ProjectStatementKey.newBuilder().setProjectId(projectIdLeft).setStatementId(statementIdLeft).build();
+        var vLeft = ProjectStatementValue.newBuilder()
+                .setProjectId(projectIdLeft)
+                .setStatementId(statementIdLeft)
+                .setStatement(
+                        StatementEnrichedValue.newBuilder()
+                                .setSubjectId(subjectIdLeft)
+                                .setObjectId(objectIdLeft)
+                                .setPropertyId(propertyIdLeft)
+                                .build())
+                .setDeleted$1(true)
+                .build();
+        projectStatementWithEntityTopic.pipeInput(kLeft, vLeft);
+
+        var kRight = ProjectStatementKey.newBuilder().setProjectId(projectIdRight).setStatementId(statementIdRight).build();
+        var vRight = ProjectStatementValue.newBuilder()
+                .setProjectId(projectIdRight)
+                .setStatementId(statementIdRight)
+                .setStatement(
+                        StatementEnrichedValue.newBuilder()
+                                .setSubjectId(subjectIdRight)
+                                .setObject(NodeValue.newBuilder()
+                                        .setClassId(0)
+                                        .setAppellation(Appellation.newBuilder()
+                                                .setFkClass(0)
+                                                .setString(objectAppellationStringRight)
+                                                .build()
+                                        ).build()
+                                )
+                                .setPropertyId(propertyIdRight)
+                                .build())
+                .setDeleted$1(true)
+                .build();
+        projectStatementWithLiteralTopic.pipeInput(kRight, vRight);
+
+        var outRecords = outputTopic.readKeyValuesToMap();
+
+        var expectedKey = ProjectRdfKey.newBuilder()
+                .setProjectId(projectIdLeft)
+                .setTurtle("<http://geovistory.org/resource/" + subjectIdLeft + "> <http://www.w3.org/2002/07/owl#sameAs> <" + objectAppellationStringRight + "> .")
+                .build();
+
+        var record = outRecords.get(expectedKey);
+        assertThat(record.getOperation()).isEqualTo(Operation.delete);
+    }
+
+    /**
+     * The goal of this test is to check if the operation value "insert" is correctly set in the output topic
+     */
+    @Test
+    void testOperationValueInsert() {
+
+        var projectIdLeft = 1;
+        var subjectIdLeft = "i1";
+        var statementIdLeft = 111;
+        var objectIdLeft = "i200";
+        var propertyIdLeft = 1943;
+
+        var projectIdRight = 1;
+        var subjectIdRight = "i200";
+        var statementIdRight = 999;
+        var objectAppellationStringRight = "https://wikidata/i345";
+        var propertyIdRight = 1843;
+
+        var kLeft = ProjectStatementKey.newBuilder().setProjectId(projectIdLeft).setStatementId(statementIdLeft).build();
+        var vLeft = ProjectStatementValue.newBuilder()
+                .setProjectId(projectIdLeft)
+                .setStatementId(statementIdLeft)
+                .setStatement(
+                        StatementEnrichedValue.newBuilder()
+                                .setSubjectId(subjectIdLeft)
+                                .setObjectId(objectIdLeft)
+                                .setPropertyId(propertyIdLeft)
+                                .build())
+                .setDeleted$1(false)
+                .build();
+        projectStatementWithEntityTopic.pipeInput(kLeft, vLeft);
+
+        var kRight = ProjectStatementKey.newBuilder().setProjectId(projectIdRight).setStatementId(statementIdRight).build();
+        var vRight = ProjectStatementValue.newBuilder()
+                .setProjectId(projectIdRight)
+                .setStatementId(statementIdRight)
+                .setStatement(
+                        StatementEnrichedValue.newBuilder()
+                                .setSubjectId(subjectIdRight)
+                                .setObject(NodeValue.newBuilder()
+                                        .setClassId(0)
+                                        .setAppellation(Appellation.newBuilder()
+                                                .setFkClass(0)
+                                                .setString(objectAppellationStringRight)
+                                                .build()
+                                        ).build()
+                                )
+                                .setPropertyId(propertyIdRight)
+                                .build())
+                .setDeleted$1(true)
+                .build();
+        projectStatementWithLiteralTopic.pipeInput(kRight, vRight);
+
+        var outRecords = outputTopic.readKeyValuesToMap();
+
+        var expectedKey = ProjectRdfKey.newBuilder()
+                .setProjectId(projectIdLeft)
+                .setTurtle("<http://geovistory.org/resource/" + subjectIdLeft + "> <http://www.w3.org/2002/07/owl#sameAs> <" + objectAppellationStringRight + "> .")
+                .build();
+
+        var record = outRecords.get(expectedKey);
+        assertThat(record.getOperation()).isEqualTo(Operation.insert);
+    }
+
+    /**
+     * The goal of this test is to check if the URI used for the sameAs relation is correctly validated by the regex
+     * In this case, a malformed URI is used, so we expect the output topic to be empty
+     */
+    @Test
+    void testWrongUriRegexValue() {
+
+        var projectIdLeft = 1;
+        var subjectIdLeft = "i1";
+        var statementIdLeft = 111;
+        var objectIdLeft = "i200";
+        var propertyIdLeft = 1943;
+
+        var projectIdRight = 1;
+        var subjectIdRight = "i200";
+        var statementIdRight = 999;
+        var objectAppellationStringRight = "wikidata/i345"; //wrong URI used to test the regex
+        var propertyIdRight = 1843;
+
+        var kLeft = ProjectStatementKey.newBuilder().setProjectId(projectIdLeft).setStatementId(statementIdLeft).build();
+        var vLeft = ProjectStatementValue.newBuilder()
+                .setProjectId(projectIdLeft)
+                .setStatementId(statementIdLeft)
+                .setStatement(
+                        StatementEnrichedValue.newBuilder()
+                                .setSubjectId(subjectIdLeft)
+                                .setObjectId(objectIdLeft)
+                                .setPropertyId(propertyIdLeft)
+                                .build())
+                .setDeleted$1(false)
+                .build();
+        projectStatementWithEntityTopic.pipeInput(kLeft, vLeft);
+
+        var kRight = ProjectStatementKey.newBuilder().setProjectId(projectIdRight).setStatementId(statementIdRight).build();
+        var vRight = ProjectStatementValue.newBuilder()
+                .setProjectId(projectIdRight)
+                .setStatementId(statementIdRight)
+                .setStatement(
+                        StatementEnrichedValue.newBuilder()
+                                .setSubjectId(subjectIdRight)
+                                .setObject(NodeValue.newBuilder()
+                                        .setClassId(0)
+                                        .setAppellation(Appellation.newBuilder()
+                                                .setFkClass(0)
+                                                .setString(objectAppellationStringRight)
+                                                .build()
+                                        ).build()
+                                )
+                                .setPropertyId(propertyIdRight)
+                                .build())
+                .setDeleted$1(true)
+                .build();
+        projectStatementWithLiteralTopic.pipeInput(kRight, vRight);
+
+        projectStatementWithLiteralTopic.pipeInput(kRight, vRight);
+
+        assertThat(outputTopic.isEmpty()).isTrue();
+    }
 }
