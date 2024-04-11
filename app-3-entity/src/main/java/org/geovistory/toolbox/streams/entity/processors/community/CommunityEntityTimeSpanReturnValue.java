f5cc58a1bb5c94ab2b5512b6cb14688c0e8db401
joschne <jonas.schneider@balcab.ch>
2024-04-11 21:47:57 +0200

    remove old entity time span processors

--- app-3-entity/src/main/java/org/geovistory/toolbox/streams/entity/processors/community/CommunityEntityTimeSpanReturnValue.java
+++
@@ -1,9 +0,0 @@
-package org.geovistory.toolbox.streams.entity.processors.community;
-
-import org.apache.kafka.streams.kstream.KStream;
-import org.geovistory.toolbox.streams.avro.CommunityEntityKey;
-import org.geovistory.toolbox.streams.avro.TimeSpanValue;
-
-public record CommunityEntityTimeSpanReturnValue(
-        KStream<CommunityEntityKey, TimeSpanValue> communityEntityTimeSpanStream) {
-}
--- app-3-entity/src/main/java/org/geovistory/toolbox/streams/entity/processors/project/ProjectEntityTimeSpanReturnValue.java
+++
@@ -1,8 +0,0 @@
-package org.geovistory.toolbox.streams.entity.processors.project;
-
-import org.apache.kafka.streams.kstream.KStream;
-import org.geovistory.toolbox.streams.avro.ProjectEntityKey;
-import org.geovistory.toolbox.streams.avro.TimeSpanValue;
-
-public record ProjectEntityTimeSpanReturnValue(KStream<ProjectEntityKey, TimeSpanValue> projectEntityTimeSpanStream) {
-}
